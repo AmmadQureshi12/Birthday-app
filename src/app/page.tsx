0f05888c615ea2a47a670b40c1acadb4c06e989d
Ammad Qureshi <ammadq196@gmail.com>
2025-02-27 00:53:15 +0500

    b-day

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Birthdayapp from '@/components/birthdayapp'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Birthdayapp/>
+    </div>
+  )
+}
+
+export default page
